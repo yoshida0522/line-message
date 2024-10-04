3cdd992fafbb4c7328b0eb6c547b75f14cf12a9c
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-04 22:34:26 +0900

    .env追加

--- src/app/api/lineMessage/route.ts
+++ src/app/api/lineMessage/route.ts
@@ -1,49 +1,40 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-const CHANNEL_ACCESS_TOKEN =
-  "HrdWhF6LCombABpNRZDkV/fsXR+WcotUAhp7rApxZzHh96E+CWYExMJ/NimYKjIIDvHpJS9e5GwdSed8ylLDbZ/rZnBLDWJd6yY2mxhwODrt0x/OUb6XAo8WowMRaTeYShjX3S1CPwlcRcS0oYldRAdB04t89/1O/w1cDnyilFU="; // LINEチャネルアクセストークンをここに追加
+const CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
-
-  const { name, email } = req.body;
-
-  // LINE Messaging APIに送るメッセージ内容を定義
-  const message = {
-    to: "U6a934c65da47bd1a06d768e5e35da61f", // 送信相手のユーザーIDを指定
-    messages: [
-      {
-        type: "text",
-        text: `新しいフォームの送信があります。\n名前: ${name}\nメールアドレス: ${email}`,
-      },
-    ],
-  };
-
-  // LINE APIにPOSTリクエストを送信
+export async function POST(req: NextRequest) {
   try {
+    const { name, email } = await req.json();
+
+    const message = {
+      to: "U6a934c65da47bd1a06d768e5e35da61f",
+      messages: [
+        {
+          type: "text",
+          text: `新しいフォームの送信があります。\n名前: ${name}\nメールアドレス: ${email}`,
+        },
+      ],
+    };
+
     const response = await fetch("https://api.line.me/v2/bot/message/push", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${CHANNEL_ACCESS_TOKEN}`, // アクセストークンをヘッダーに設定
+        Authorization: `Bearer ${CHANNEL_ACCESS_TOKEN}`,
       },
       body: JSON.stringify(message),
     });
 
     if (response.ok) {
-      return res.status(200).json({ message: "メッセージを送信しました" });
+      return NextResponse.json({ message: "メッセージを送信しました" });
     } else {
-      return res
-        .status(response.status)
-        .json({ message: "メッセージ送信に失敗しました" });
+      return NextResponse.json(
+        { message: "メッセージ送信に失敗しました" },
+        { status: response.status }
+      );
     }
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "サーバーエラー" });
+    return NextResponse.json({ message: "サーバーエラー" }, { status: 500 });
   }
 }
