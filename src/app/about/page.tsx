669b0ab78d23ea4f221df857e1ea580e2fa0acdf
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-04 21:38:37 +0900

    ページを追加しました

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>このアプリについて</h1>
+      <p>
+        このアプリは、LINEメッセージAPIを使って、フォームに入力されたデータをLINEメッセージとして送信する機能を持っています。
+      </p>
+    </div>
+  );
+}
