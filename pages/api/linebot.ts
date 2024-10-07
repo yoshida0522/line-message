901f846ce78e93855125a53acf95974d2bda48b3
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-07 20:45:15 +0900

    .envからトークンを取得

--- pages/api/linebot.ts
+++ pages/api/linebot.ts
@@ -2,9 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import * as line from "@line/bot-sdk";
 
 const config = {
-  channelAccessToken:
-    "RVSL1mJWImhKz76nhCuzeXMYMbQDHFELcWvqE5rioOa+Q23FJY80IRs4hkrG/WAuDvHpJS9e5GwdSed8ylLDbZ/rZnBLDWJd6yY2mxhwODoKCnpxM9RaixcdR21ZGgURnUuP0r6kp4c0e5j5mFFBsAdB04t89/1O/w1cDnyilFU=",
-  channelSecret: "dacfaefe36947703f82bc0e8ae9f5611",
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN || "",
+  channelSecret: process.env.LINE_CHANNEL_ACCESS_TOKEN || "",
 };
 
 const client = new line.Client(config);
